ff9e1968cd0981b2802ce711f669256c7f4ac8b7
Jordan S <jpstanway@gmail.com>
2021-01-01 15:19:57 -0800

    update error handling for db connection

--- database/db.ts
+++ database/db.ts
@@ -3,17 +3,19 @@ import mongoose from 'mongoose';
 
 const connection: any = {};
 const DB_URI: string = env.get('DB_URI').required().asString();
-console.log(DB_URI);
+
 const dbConnect = async () => {
   if (connection.isConnected) return true;
-
-  const db = await mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  });
-
-  connection.isConnected = db.connections[0].readyState;
+  try {
+    const db = await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export default dbConnect;
